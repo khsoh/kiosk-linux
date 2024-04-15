f190b38b0e6d17fa70fc9d86cc3a867f0bc5a5d6
K H Soh <2169449+khsoh@users.noreply.github.com>
2024-04-15 16:37:07 +0800

    Changed kiosk setup to use XDG autostart

--- setup_kiosk.sh
+++ setup_kiosk.sh
@@ -3,46 +3,24 @@
 SCRIPTNAME=$(readlink -f ${BASH_SOURCE[0]})
 pushd "$(dirname $SCRIPTNAME)" > /dev/null
 
-if [[ ! -d /opt/kiosk ]]; then
-    sudo mkdir -p /opt/kiosk
-fi
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+read -p "Kiosk URL: " KIOSKURL
 
-cat << __cleanup_kiosk > ./cleanup_kiosk.sh
+cat << __startup_kiosk > $XDG_CONFIG_HOME/autostart/startup_kiosk.sh
 #!/bin/bash
 if [[ -f \$HOME/snap/chromium/common/chromium/Default/Preferences ]]; then
     sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' \$HOME/snap/chromium/common/chromium/Default/Preferences
     sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' \$HOME/snap/chromium/common/chromium/Default/Preferences
 fi
-__cleanup_kiosk
-sudo mv -f ./cleanup_kiosk.sh /opt/kiosk/cleanup_kiosk.sh
-sudo chown root /opt/kiosk/cleanup_kiosk.sh
-sudo chgrp root /opt/kiosk/cleanup_kiosk.sh
-sudo chmod +x /opt/kiosk/cleanup_kiosk.sh
+/usr/bin/chromium-browser -noerrdialogs --disable-infobars --no-first-run --start-maximized --kiosk $KIOSKURL &
+__startup_kiosk
 
-read -p "Kiosk URL: " KIOSKURL
-
-cat << __kioskservice > ./kiosk.service
-[Unit]
-Description=Chromium Kiosk
-Wants=graphical.target
-After=graphical.target
-
-[Service]
-Environment=DISPLAY=:0
-Type=simple
-ExecStartPre=/opt/kiosk/cleanup_kiosk.sh
-ExecStart=/usr/bin/chromium-browser -noerrdialogs --disable-infobars --no-first-run --start-maximized --kiosk $KIOSKURL
-Restart=no
-User=$USER
-Group=$USER
-
-[Install]
-WantedBy=graphical.target
-__kioskservice
-sudo mv -f ./kiosk.service /lib/systemd/system/kiosk.service
-sudo chown root /lib/systemd/system/kiosk.service
-sudo chgrp root /lib/systemd/system/kiosk.service
-sudo systemctl enable kiosk
+cat << __kiosk_desktop > $XDG_CONFIG_HOME/autostart/kiosk.desktop
+[Desktop Entry]
+Type=Application
+Name=Kiosk
+Exec=bash -e -c "source \\\\\$HOME/.config/autostart/startup_kiosk.sh"
+__kiosk_desktop
 
 popd > /dev/null
 
