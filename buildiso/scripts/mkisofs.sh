82f65c1e78f15365ba8ae256dcaf03af6c89b213
K H Soh <hju37823@outlook.com>
2025-10-20 08:33:03 +0800

    Added comments for .xbindkeysrc to explain use of commands to generate mouse clicks Added mkisofs.sh script to create ISO image

---
+++ buildiso/scripts/mkisofs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+FULLDATE=$(env TZ="Asia/Singapore" date +"%Y%m%d-%H%M%S%2N")
+IFS='-' read -r STRDATE STRTIME <<< "$FULLDATE"
+
+sudo xorriso -as mkisofs -R -r -J -joliet-long -l -cache-inodes -iso-level 3 -A "Debian Live" -publisher "ZBAV Tech Lead" -V "ZBAV_Kiosk_Image" --modification-date=$STRDATE$STRTIME -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -isohybrid-gpt-basdat -isohybrid-apm-hfsplus -o $1 $2
