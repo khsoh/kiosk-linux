3e40a80de93d8fa8332bd3a9d27ea6348932e686
K H Soh <hju37823@outlook.com>
2025-10-23 07:22:39 +0800

    Fixed bug in 20mksqfs_iso.sh script

--- buildiso/scripts/20mksqfs_iso.sh
+++ buildiso/scripts/20mksqfs_iso.sh
@@ -44,8 +44,8 @@ grep -Ff <(awk '{print $2}' ./sha256sum.txt) <(find . -type f -print0 | sort -z 
 mv -f shx.txt sha256sum.txt
 popd
 
-FULLDATE=\$(env TZ="Asia/Singapore" date +"%Y%m%d-%H%M%S%2N")
-IFS='-' read -r STRDATE STRTIME <<< "\$FULLDATE"
+FULLDATE=$(env TZ="Asia/Singapore" date +"%Y%m%d-%H%M%S%2N")
+IFS='-' read -r STRDATE STRTIME <<< "$FULLDATE"
 
 xorriso -as mkisofs -R -r -J -joliet-long -l -cache-inodes -iso-level 3 -A "Debian Live" -publisher "ZBAV Tech Lead" -V "ZBAV_KIOSK" --modification-date=$STRDATE$STRTIME -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -isohybrid-gpt-basdat -isohybrid-apm-hfsplus -o $2 $ISOPATH
 
