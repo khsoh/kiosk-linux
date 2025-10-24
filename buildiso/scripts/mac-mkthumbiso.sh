fd4039e7d1cc0434022c8ec6450f6fbb8de838f2
K H Soh <hju37823@outlook.com>
2025-10-24 14:46:01 +0800

    Added code to unmount thumbdrive before writing and eject after writing

--- buildiso/scripts/mac-mkthumbiso.sh
+++ buildiso/scripts/mac-mkthumbiso.sh
@@ -1,7 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 if diskutil list $2 | head -1 | grep external > /dev/null ; then
-    sudo dd if=$1 of=$2 bs=4M status=progress
+    BDISK="${2/rdisk/disk}"
+    RDISK="${BDISK/disk/rdisk}"
+    # Unmount the buffered disk first
+    if df -h /dev/disk4 >/dev/null 2>&1; then
+        diskutil unmountDisk $BDISK
+    fi
+
+    # Write to raw disk
+    sudo dd if=$1 of=$RDISK bs=4M status=progress
+
+    diskutil eject $BDISK
+
 else
     echo $2 is not an external device - will not run command
 fi
